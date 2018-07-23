240d2b7321258e4f155e7cca6322d542d633fd2d
Magda Kłopotek <magda.klopotek@gmail.com>
2018-07-23 12:16:05 +0200

    class added

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+console.log(Component)
+
+const appClassPrefix = 'App'
+
+const App = () => (
+      <div className={`${appClassPrefix}`}>
+        <header className={`${appClassPrefix}-header`}>
+          <img src={logo} className={`${appClassPrefix}-logo`} alt={'logo'} />
+          <h1 className={`${appClassPrefix}-title`}>Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className={`${appClassPrefix}-intro`}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
-  }
-}
+)
 
 export default App;
